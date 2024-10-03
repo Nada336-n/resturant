97361b4f09fffcdf23740363461d0267b8ca1ba9
Nada336-n <nada1471814@gmail.com>
2024-10-04 02:31:53 +0300

    Add files via upload

---
+++ home.js
@@ -0,0 +1,27 @@
+let recipesSection = document.querySelector(".recipes"); 
+let r = new XMLHttpRequest(); 
+r.onload = () => { 
+    if (r.readyState === 4) { 
+        if (r.status == 200) { 
+            let response = JSON.parse(r.responseText); 
+            let recipes = response.recipes; 
+            recipes.map((recipe) => { 
+                recipesSection.innerHTML += ` 
+                    <div class="card" data-id="${recipe.id}"> 
+                        <img src="${recipe.image}" alt=""> 
+                        <h4>${recipe.name}</h4> 
+                        <div class="stars"> 
+                            <i class="fa-solid fa-star"></i> 
+                            <i class="fa-solid fa-star"></i> 
+                            <i class="fa-solid fa-star"></i> 
+                            <i class="fa-solid fa-star"></i> 
+                            <i class="fa-regular fa-star"></i> 
+                        </div> 
+                    </div> 
+                `; 
+            }); 
+        } 
+    } 
+}; 
+r.open("GET", "https://dummyjson.com/recipes?sortBy=name&order=asc", true); 
+r.send(); 
